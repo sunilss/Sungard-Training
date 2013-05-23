9bdf203b12fb03659848f40a200a2ac015143050
Magesh <tkmagesh77@gmail.com>
2013-05-23 18:04:56 +0530

    Updated

---
+++ Day-1/jQuery/ProductsList.js
@@ -0,0 +1,5 @@
+var products = [{Id : 101, Name : "Pen", Cost : 12, Units : 10, Category : 1},
+{Id : 105, Name : "Hen", Cost : 20, Units : 20, Category : 1},
+{Id : 103, Name : "Ken", Cost : 70, Units : 35, Category : 2},
+{Id : 109, Name : "Den", Cost : 40, Units : 40, Category : 1},
+{Id : 107, Name : "Ten", Cost : 90, Units : 9, Category : 2}]
