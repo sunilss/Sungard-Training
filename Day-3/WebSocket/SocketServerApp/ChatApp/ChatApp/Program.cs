e1b43e7e0abaa6ab32b717f08ff998658e172d4b
Magesh <tkmagesh77@gmail.com>
2013-06-06 17:46:28 +0530

    Web Sockets

---
+++ Day-3/WebSocket/SocketServerApp/ChatApp/ChatApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fleck;
+namespace ChatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IWebSocketConnection> connections = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://localhost:9090/ChatServer");
+            server.Start(con =>
+                {
+                    con.OnOpen += () =>
+                        {
+                            connections.Add(con);
+                            Console.WriteLine("A new connection is established");
+                        };
+                    con.OnClose += () =>
+                        {
+                            connections.Remove(con);
+                            Console.WriteLine("An existing connection is destroyed");
+                        };
+                    con.OnMessage += s =>
+                        {
+                            Console.WriteLine("A new message [{0}] is received", s);
+                            foreach(var c in connections)
+                                if (c != con) c.Send(s);
+                        };
+                });
+            Console.WriteLine("Press any text to send to the client or ENTER to shutdown");
+            var input = string.Empty;
+            while ((input = Console.ReadLine()) != "EXIT")
+            {
+                foreach(var c in connections)
+                    c.Send(input);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Day-3/WebWorkers/doWorkWorker.js
+++ Day-3/WebWorkers/doWorkWorker.js
@@ -1,12 +1,19 @@
 function doWork(){
 	for(var i=0;i<100000;i++)
+	{
 		for(var j=0;j<1000;j++)
 			for(var k=0;k<100;k++)
-			{}
+			{
+
+			}
+		if (i % 1000 === 0){
+			self.postMessage({type : "progress", percentCompleted : i /1000});
+		}
+	}
 }
 self.onmessage = function(evt){
 	if (evt.data === "start"){
 		doWork();
-		self.postMessage("completed");
+		self.postMessage({type : "completed"});
 	}
 }
